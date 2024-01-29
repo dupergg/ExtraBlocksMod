96b6e8e45e3a389c255ac1a6d52998c362765bf5
Bryce Doperalski <brycedoperalski@gmail.com>
2024-01-29 15:09:19 -0600

    initial template commit

---
+++ src/client/java/duper/extrablocks/ExtraBlocksClient.java
@@ -0,0 +1,10 @@
+package duper.extrablocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraBlocksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
